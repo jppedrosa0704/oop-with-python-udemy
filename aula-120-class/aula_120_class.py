31699abd2ddda1f8dbdef2abb68b810d7fd18c16
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-23 17:44:49 +0000

    Refactor Pessoa class and instance creation
    Updated Pessoa class initialization and modified instances.

--- aula-120-class/aula_120_class.py
+++ aula-120-class/aula_120_class.py
@@ -9,19 +9,21 @@
 # print(string.upper())
 # print(isinstance(string, str))
 class Pessoa:
-    ...
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
 
 
-p1 = Pessoa('Luiz', 'Otávio')
-p1.nome = 'Luiz'
-p1.sobrenome = 'Otávio'
 
-p2 = Pessoa('Maria', 'Joana')
-p2.nome = 'Maria'
-p2.sobrenome = 'Joana'
+p1 = Pessoa('João', 'Pedrosa')
+# p1.nome = 'João'
+# p1.sobrenome = 'Pedrosa'
+
+p2 = Pessoa('Ana', 'Medeiros')
+# p2.nome = 'Ana'
+# p2.sobrenome = 'Medeiros'
 
 print(p1.nome)
 print(p1.sobrenome)
-
-print(p2.nome)
+print(p2.nome   )
 print(p2.sobrenome)
