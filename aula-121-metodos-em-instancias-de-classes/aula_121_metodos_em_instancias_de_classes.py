fe41432531d3e8c33bba393370bd04124adebaac
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-23 18:21:39 +0000

    Add files via upload

--- aula-121-metodos-em-instancias-de-classes/aula_121_metodos_em_instancias_de_classes.py
+++ aula-121-metodos-em-instancias-de-classes/aula_121_metodos_em_instancias_de_classes.py
@@ -1,1 +1,18 @@
+#Metódos em instâncias de classes python.
+#Hard coded - é ago que foi escrito diretamente no código
 
+class carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando.')
+
+
+fusca = carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = carro(nome='Celta') #arumento nomeado
+print(celta.nome)
+celta.acelerar()
