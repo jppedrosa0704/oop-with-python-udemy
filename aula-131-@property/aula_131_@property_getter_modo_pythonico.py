0434b25b4c40b9fb21be22a5f03d94f63ee0ec5c
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-30 12:01:22 +0100

    Add files via upload

--- aula-131-@property/aula_131_@property_getter_modo_pythonico.py
+++ aula-131-@property/aula_131_@property_getter_modo_pythonico.py
@@ -1,1 +1,52 @@
+# @property - um getter no modo Pythônico
+# getter - um método para obter um atributo
+# cor -> get_cor()
+# modo pythônico - modo do Python de fazer coisas
+# @property é uma propriedade do objeto, ela
+# é um método que se comporta como um
+# atributo 🤯 🤯 🤯
+# Geralmente é usada nas seguintes situações:
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Código cliente - é o código que usa seu código
+
+class Caneta:
+    def __init__(self, cor):
+        self.cor_tinta = cor
+    
+    @property
+    def cor(self):
+        #property
+        return self.cor_tinta
+    
+    @property
+    def cor_tampa(self):
+        return 'Marrom'
+    
+caneta = Caneta('Azul')
+print(caneta.cor)
+print(caneta.cor_tampa)
+    
+
+
+# class Caneta:
+#     def __init__(self, cor):
+#         self.cor = cor
+
+#     def get_cor(self):
+#         #GET COR
+#         return self.cor
+    
+# caneta = Caneta('azul')
+# print(caneta.get_cor())
+
+
+
+        
+        
+    
+
+
 
