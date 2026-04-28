d6f91cfb7b83eb1424c8ce75dc142e8fb70189c8
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-04-28 15:17:27 +0100

    Add files via upload

--- 146-criando--exception/aula_146_Criando_exception_POO.py
+++ 146-criando--exception/aula_146_Criando_exception_POO.py
@@ -1,1 +1,33 @@
+# Criando Exceptions em Python Orientado a Objetos
+# Para criar uma Exception em Python, você só
+# precisa herdar de alguma exceção da linguagem.
+# A recomendação da doc é herdar de Exception.
+# https://docs.python.org/3/library/exceptions.html
+# Criando exceções (comum colocar Error ao final)
+# Levantando (raise) / Lançando (throw) exceções
+# Relançando exceções
+# Adicionando notas em exceções (3.11.0)
+class MeuError(Exception):
+    ...
 
+
+class Outro_error(Exception):
+    ...
+
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    exception_.add_note('Olha a nota 1')
+    exception_.add_note('Você errou isso...')
+    raise exception_
+
+try:
+    # 1/0
+    levantar()
+except (MeuError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = Outro_error('Vou lançar outro erro.')
+    exception_.add_note('mais uma nota')
+    exception_.__notes__ += error.__notes__.copy()
+    raise exception_ from error
